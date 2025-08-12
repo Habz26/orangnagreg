16bda1fc143d0093e5c1b6bbc5c58f805646af3d
Muhammad Sihabuddin Arroyan <muhammad.sihabuddin259@smk.belajar.id>
2025-08-12 12:49:11 +0700

    tambah tabel barang

--- database/migrations/2025_08_12_052221_create_barangs_table.php
+++ database/migrations/2025_08_12_052221_create_barangs_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('barangs', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            $table->string('nama');
         });
     }
 
