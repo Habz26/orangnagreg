db0443304a2e170e9930f533bcf8bbb9c727e0f6
Muhammad Sihabuddin Arroyan <muhammad.sihabuddin259@smk.belajar.id>
2025-08-13 09:31:52 +0700

    mengubah isi tabel barang

--- database/migrations/2025_08_12_052221_create_barangs_table.php
+++ database/migrations/2025_08_12_052221_create_barangs_table.php
@@ -14,6 +14,8 @@ return new class extends Migration
         Schema::create('barangs', function (Blueprint $table) {
             $table->id();
             $table->string('nama_barang');
+            $table->integer('qty');
+            $table->double('harga');
             $table->timestamps();
         });
     }
